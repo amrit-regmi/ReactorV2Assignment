9074fbaad27d8bbc9b96950c54dd110c17f46a43
amrit <rit.regmi@gmail.com>
2020-11-29 14:48:17 +0200

    More Documentation

--- src/Components/ItemsPage.js
+++ src/Components/ItemsPage.js
@@ -1,17 +1,17 @@
 import React, { useEffect, useState } from 'react'
 import { Dimmer, Loader, Segment } from 'semantic-ui-react'
 import { getAvailablilityByManufacturer, getProductsByType } from '../services'
-import DataTable from './DataTable/DataTable'
+import DataTable from './DataTable'
 import ErrorMessage from './Error'
 
 const ItemsPage = ({ item }) => {
-  const [products,setProducts]= useState({})
-  const [manufacturers,setManufacturers] =useState({})
-  const [data, setData] = useState([])
-  const [errors,setErrors] = useState([])
-  const [loading,setLoading] = useState(false)
-  const [refreshing, setRefreshing] = useState(false)
-  const cacheDuration = 300000 // 5 min cache
+  const [products,setProducts]= useState({}) // Stores all products data
+  const [manufacturers,setManufacturers] =useState({}) // Stores all manufacturers data and will be shared for each item
+  const [data, setData] = useState([]) // Stores data for current page
+  const [errors,setErrors] = useState([]) // Errors, stores error and its respective refetch function
+  const [loading,setLoading] = useState(false) //Stores product items loading state
+  const [refreshing, setRefreshing] = useState(false) //Stores refreshing state when the request is sent on background
+  const cacheDuration = 300000 // 5 min ApiCache
 
   /**
  *Function to check if the data needs to be refetched,
@@ -57,10 +57,14 @@ const ItemsPage = ({ item }) => {
     try{
       setRefreshing(true)
       const availabilityData =   await  getAvailablilityByManufacturer(manufacturer)
+
+      /**Check If the received data is invalid*/
       if(availabilityData.code !== 200 || !Array.isArray(availabilityData.response)){
         requestData = { error:'Error Fetching',data:[] }
         setErrors(errors => {return [...errors,{ message:`Failed to load availability data for Manufacturer ${manufacturer} ` , retry: () => getAvailability(manufacturer) }]})
-      }else{
+      }
+      else{
+        /**Data is valid */
         requestData = { error: false, data:availabilityData.response , lastRetrieved: Date.now() }
 
       }}
@@ -71,24 +75,26 @@ const ItemsPage = ({ item }) => {
     }
     finally
     {
+      /**Add the manufacturer data to manufacturers state */
       setManufacturers((manufacturers) => {return { ...manufacturers, [manufacturer]:requestData }})
       setRefreshing(false)
     }
   }
 
+  /**Fires when the page data changes  */
   useEffect(() => {
     setLoading(true)
     let timeout
     const requestProducts = async () => {
       const dataStale = products[item] ? isStale (products[item].lastRetrieved): true //If data is set and notStale, store value when it needs to be refetched
-
-      /**Refetch request only after cache has expired */
       if(dataStale === true){
+        /*Refetch immediately and time another refetch for cache duration  */
         setRefreshing(true)
         await fetchProducts(item)
         timeout = setTimeout( requestProducts, cacheDuration )
 
       }else{
+        /*Render page from cached data and refetch request only after cache has expired */
         setData(products[item].data)
         timeout = setTimeout( requestProducts, dataStale )
         setLoading(false)
@@ -108,8 +114,9 @@ const ItemsPage = ({ item }) => {
   },[item])
 
 
+  /**Fires when the products data changes  ie. when the new request is made for product type*/
   useEffect(  () => {
-    /*If data is not set */
+    /*If page data is not set */
     if(!data.length){
       return null
     }
@@ -118,20 +125,23 @@ const ItemsPage = ({ item }) => {
     let timeOuts = []
     const manufacturerList = new Set()
 
-
+    /**For each product listed on page get manufacturer and add to list */
     data.forEach(product => {
       manufacturerList.add(product.manufacturer)
     })
 
     const requestAvailability =  () => {
-
+      /**For each manufacturer check if the data has expired , fetch accordingly */
       manufacturerList.forEach(async(manufacturer) => {
-
         const dataStale = manufacturers[manufacturer]? isStale (manufacturers[manufacturer].lastRetrieved) || manufacturers[manufacturer].error !== false : true
-
         const timeOutDuration = dataStale === true? 0 : dataStale
 
-        timeOuts.push(setTimeout (() => getAvailability(manufacturer),timeOutDuration)) //If the data isstale or errror fetch immediately else wait until cache expires
+        /**
+         *If the data isstale or errror fetch immediately timeoutDuration will be 0
+         *else wait until cache expires timeoutDuration will be timeuntil cache expires
+         *store the timeoutId for each request in array so each can be cleared when components unmounts
+         */
+        timeOuts.push(setTimeout (() => getAvailability(manufacturer),timeOutDuration))
         setRefreshing(false)
       })
     }
@@ -179,6 +189,7 @@ const ItemsPage = ({ item }) => {
           </Dimmer>
         </Segment>
       }
+
       <DataTable pageData ={data} manufacturersData={manufacturers} />
     </>
   )

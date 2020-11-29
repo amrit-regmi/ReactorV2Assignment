09b84145d6dc49fb9bd3a24d2218564261526b67
amrit <rit.regmi@gmail.com>
2020-11-29 14:49:17 +0200

    rename from DataTable to index

--- src/Components/DataTable/index.js
+++ src/Components/DataTable/index.js
@@ -5,6 +5,7 @@ import { FixedSizeList as List } from 'react-window'
 
 const DataTable = ( { pageData, manufacturersData  }) => {
 
+  /** Component to display Individual Product Row*/
   const DataGridRows = ({ index, style ,data }) => {
     return  (
       <Grid celled textAlign='center' style={{ ...style, margin:0 , backgroundColor: index % 2? 'whitesmoke':'' }}>
@@ -16,18 +17,21 @@ const DataTable = ( { pageData, manufacturersData  }) => {
 
     pageData.length > 0 &&
     <>
+      {/*Table Header Grid*/}
       <Grid celled textAlign='center'>
         <Grid.Row style={{ background:'aliceblue', color:'#000000de' ,borderBottom:'1px solid #2224261a' , fontWeight:'bold' ,paddingRight: '10px' }} columns='5'>
-          <Grid.Column >Name</Grid.Column>
-          <Grid.Column>Manufacturer</Grid.Column>
-          <Grid.Column>Available Colours</Grid.Column>
-          <Grid.Column>Price</Grid.Column>
-          <Grid.Column>Availability</Grid.Column>
+          <Grid.Column width='4'>Name</Grid.Column>
+          <Grid.Column width='3'>Manufacturer</Grid.Column>
+          <Grid.Column width='3'>Available Colours</Grid.Column>
+          <Grid.Column width='2'>Price</Grid.Column>
+          <Grid.Column width='2'>Availability</Grid.Column>
         </Grid.Row>
       </Grid>
 
-
+      {/** Data Grid Wrapper*/}
       <Grid columns='5' celled style={{ margin:'0' }}>
+
+        {/**Windowing for smooth page laoding */}
         < List
           height={window.innerHeight-150}
           itemCount={pageData.length}

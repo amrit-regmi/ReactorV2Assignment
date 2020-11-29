77c5a7038d9643d00ffbf2730e58538cd7a6a80e
amrit <rit.regmi@gmail.com>
2020-11-29 14:47:50 +0200

    Uichanges, Availbility data is now stored as state on GridRow and drilled to AvailabilityComponent , Added More Documentation

--- src/Components/DataTable/AvailabilityColumn.js
+++ src/Components/DataTable/AvailabilityColumn.js
@@ -1,44 +1,62 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Grid, Loader, Segment } from 'semantic-ui-react'
 
 
 
-const AvailabilityColumn = ({ product,manufacturers }) => {
-
-  /**
-   * Function to get the availabilty status for each product item
-   * @param {String} id productID
-   * @param {String} productManufacturer Manufacturer
-   */
-  const checkAvailability = () => {
+const AvailabilityColumn = ({ product,manufacturers, setAvailability ,availability }) => {
+  useEffect (() => {
     const manufacturer =  manufacturers[product.manufacturer]
 
-    if( manufacturer && manufacturer.data.length > 0){ //If manufacturer information exixts
+    if(manufacturers && manufacturers[product.manufacturer] && manufacturers[product.manufacturer].error ){ //If manufacturer information exists and error is set
+      setAvailability('ERROR')
+      return
+    }
+
+    if( manufacturer && manufacturer.data.length > 0){ //If manufacturer information exists then serach record
       const record = manufacturer.data.find( item => {
         if(item.id === product.id.toUpperCase()){
           return true
         }
       })
 
-      if(record){
-        const availability = record.DATAPAYLOAD.match(/<INSTOCKVALUE>(.*?)<\/INSTOCKVALUE>/g).map(val => val.replace(/<\/?INSTOCKVALUE>/g,''))
-        return <span style={{ color: availability[0].includes('LESSTHAN')?'orange':availability[0].includes('OUTOFSTOCK')?'red':'black' }}> {availability}</span>
+      if(record){ // If record found return record with apprporiate formatting
+        let availability = record.DATAPAYLOAD.match(/<INSTOCKVALUE>(.*?)<\/INSTOCKVALUE>/g).map(val => val.replace(/<\/?INSTOCKVALUE>/g,''))
+        setAvailability(availability[0])
       }
     }
-  }
+
+  },[manufacturers])
 
   return (
-    <Grid.Column>
-      {!manufacturers || !manufacturers[product.manufacturer] ? // if the manufacturer data is not loaded
-        <Segment compact basic size='mini'>
+    <Grid.Column width='2'>
+      {!availability &&// if the manufacturer data is not loaded display loader
+        <Segment basic size='mini'>
           <Loader active size='mini' ></Loader>
-        </Segment>:
-        manufacturers && manufacturers[product.manufacturer] && manufacturers[product.manufacturer].error ? //If the request has failed
-          <span style={{ color:'red' }}>Error Fetching</span>:
-          checkAvailability()
+        </Segment>
       }
+      {
+        availability==='ERROR' &&
+        <span style={{ color:'red' }}>Error Fetching</span>
+      }
+      {
+        availability && availability.includes('OUTOFSTOCK' )&&
+        <span style={{ color:'red' }}>Out of Stock</span>
+      }
+
+      {
+        availability && availability.includes('LESSTHAN') &&
+        <span style={{ color:'orange' }}>Less than {availability.match(/\d+/)[0]}</span>
+      }
+      {
+        availability && availability.includes('INSTOCK') &&
+        'In Stock'
+      }
+
     </Grid.Column>
+
   )
+
+
 }
 
 export default AvailabilityColumn
--- src/Components/DataTable/GridRow.js
+++ src/Components/DataTable/GridRow.js
@@ -1,19 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Grid, Label } from 'semantic-ui-react'
 import AvailabilityColumn from './AvailabilityColumn'
 
 const GridRow = ({ product ,manufacturers }) => {
+  const [availbility,setAvailability] = useState('')
+
   return (
-    <Grid.Row columns='5' >
-      <Grid.Column>{product.name}</Grid.Column>
-      <Grid.Column>{product.manufacturer}</Grid.Column>
-      <Grid.Column>{
+    <Grid.Row columns='5'
+      style={
+        /*Set background color based on availability data*/
+        {
+          backgroundColor:
+        availbility.includes('LESSTHAN')?
+          'lightyellow':
+          availbility.includes('OUTOFSTOCK') || availbility.includes('ERROR')?
+            'mistyrose':
+            ''
+        }
+      }
+    >
+      <Grid.Column width='4'>{product.name}</Grid.Column>
+      <Grid.Column width='3'>{product.manufacturer}</Grid.Column>
+      <Grid.Column width='3'>{
+      /**Displaying colored labels insted of text */
         product.color && product.color.map((color) => {
           return (
             <Label size='mini' circular key={color} style={{ backgroundColor: color, border:color==='white'?'0.1em solid black':'' }}></Label>)})
       }</Grid.Column>
-      <Grid.Column>{product.price}</Grid.Column>
-      <AvailabilityColumn product={product} manufacturers = {manufacturers}/>
+      <Grid.Column width='2'>{product.price}</Grid.Column>
+      <AvailabilityColumn product={product} manufacturers = {manufacturers} setAvailability={setAvailability} availability={availbility}/>
 
 
     </Grid.Row>

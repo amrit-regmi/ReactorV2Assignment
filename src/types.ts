d0bc417f01232110659053814183647bd493ec15
amrit <rit.regmi@gmail.com>
2021-02-02 01:29:40 +0200

    modfied dataFetchStatus and lastretrieved to acept null Values / added DispatchAction Types

--- src/types.ts
+++ src/types.ts
@@ -1,24 +1,30 @@
+import { ErrorAction } from "./Reducers/errorReducer"
+import { ManufacturerAction } from "./Reducers/manufacturerReducer"
+import { ProductAction } from "./Reducers/productsReducer"
+
 export enum ProductType  {
   Beanies = 'beanies',
   Gloves = 'gloves',
   Facemasks = 'facemasks'
 }
 
-export type  DataFetchStatus  = 'error' | 'loading' | 'fetched'
+export type  DataFetchStatus  = 'error' | 'loading' | 'fetched' | null
+
+export type  DispatchActions = ErrorAction & ManufacturerAction & ProductAction
 
 export type Store = {
   products: {
     [productType in ProductType] : {
       data: any
       status: DataFetchStatus
-      lastRetrieved: Date
+      lastRetrieved: Date | null
     }
   }
   manufacturers: {
     [manufacturerName:string]: {
       data: any
       status: DataFetchStatus
-      lastRetrieved: Date    
+      lastRetrieved: Date | null 
     }
   }
   errors: {
